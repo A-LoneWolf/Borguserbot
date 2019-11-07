c38791868a563495034853dd4c9593cf239a0b09
A-LoneWolf <54808110+A-LoneWolf@users.noreply.github.com>
2019-11-07 18:01:37 +0530

    Update countdown.py

--- stdplugins/countdown.py
+++ stdplugins/countdown.py
@@ -1,4 +1,6 @@
-"""COMMAND : .cd, .scd, .padmin"""
+"""COMMAND : .cd <text >,
+ .scd <text>, 
+"""
 
 from telethon import events
 
@@ -71,29 +73,5 @@ async def timer_blankx(e):
 
   await e.edit(txt + 'NaN')
 
-
-
-
-@borg.on(events.NewMessage(outgoing=True, pattern='^\.(f?p)an '))
-
-async def timer_blankx(e):
-
- txt=e.text[7:] + '\n\n`Promoting You As Admin In` '
-
- j=5
-
- k=j
-
- for j in range(j):
-
-  await e.edit(txt + str(k))
-
-  k=k-1
-
-  await asyncio.sleep(1)
-
- if e.pattern_match.group(1) == 'f':
-
-  await e.edit("`Successfully Promoted As Admin.` ")
 
 
