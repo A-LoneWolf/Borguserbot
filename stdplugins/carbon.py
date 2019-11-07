7571d6857d801addb37c4c0a9a0f0f251ae21c03
A-LoneWolf <54808110+A-LoneWolf@users.noreply.github.com>
2019-11-07 17:28:34 +0530

    Update carbon.py

--- stdplugins/carbon.py
+++ stdplugins/carbon.py
@@ -62,7 +62,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Made by [carbon](https://carbon.now.sh)",
+         caption="Made using [carbon](https://carbon.now.sh)",
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
